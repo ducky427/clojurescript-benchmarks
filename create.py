59d62854a0ed16149fee374e9ea8b7420e74f0ae
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-06-09 17:53:29 +0100

    Updated benchmarks

--- create.py
+++ create.py
@@ -2,10 +2,17 @@
 import csv
 from jinja2 import Environment, FileSystemLoader
 
+items = ["[x 1] (identity x)",
+         "[] (symbol (quote foo))",
+         "[coll (seq arr)] (ci-reduce coll + 0)",
+         "[coll (seq arr)] (ci-reduce coll sum 0)",
+         "[coll arr] (ci-reduce coll + 0)",
+         "[coll arr] (ci-reduce coll sum 0)"]
+
 def main():
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('index.tmpl')
-    items = ["[x 1] (identity x)", "[] (symbol (quote foo))"]
+
     with open('index.html', 'wb') as f:
         f.write(template.render(items=items))
 
