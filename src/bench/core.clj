38352b5a4baa43b16225eee7d752fe6ce6586837
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-06-09 20:12:33 +0100

    Added more benchmarks

--- src/bench/core.clj
+++ src/bench/core.clj
@@ -3,13 +3,13 @@
 (defmacro benchmark-suite
   [bindings expr]
   (let [title    (str (pr-str bindings) " " (pr-str expr))]
-  `(let ~bindings
-     (let [suite# (bench.core/Suite.)]
-       (-> suite#
-           (.add ~title
-                 (fn [] ~expr))
-           (.on "cycle" (fn [event#]
-                          (let [b#    (-> event# .-target)
-                                stat# (-> b# .-stats)]
-                            (~'println (.-name b#) " || " (bench.core/get-metrics stat#)))))
-           (.run))))))
+    `(let ~bindings
+       (let [suite# (bench.core/Suite.)]
+         (-> suite#
+             (.add ~title
+                   (fn [] ~expr))
+             (.on "cycle" (fn [event#]
+                            (let [b#    (-> event# .-target)
+                                  stat# (-> b# .-stats)]
+                              (~'println (.-name b#) " || " (bench.core/get-metrics stat#)))))
+             (.run))))))
--- src/bench/core.cljs
+++ src/bench/core.cljs
@@ -18,6 +18,8 @@
                [x (gobj/get results x)])
              metrics)))
 
+(defprotocol IFoo (foo [x]))
+
 (defn init!
   []
   (benchmark-suite [x 1] (identity x))
@@ -34,6 +36,57 @@
     (benchmark-suite [coll (seq arr)] (ci-reduce coll sum 0))
     (benchmark-suite [coll arr] (array-reduce coll + 0))
     (benchmark-suite [coll arr] (array-reduce coll sum 0)))
+
+  ;; instance?
+  (benchmark-suite [coll []] (instance? PersistentVector coll))
+
+  ;; satisfies?
+  (benchmark-suite [coll (list 1 2 3)] (satisfies? ISeq coll))
+  (benchmark-suite [coll [1 2 3]] (satisfies? ISeq coll))
+
+  ;; array & string ops
+  (benchmark-suite [coll (array 1 2 3)] (seq coll))
+  (benchmark-suite [coll "foobar"] (seq coll))
+  (benchmark-suite [coll (array 1 2 3)] (first coll))
+  (benchmark-suite [coll "foobar"] (first coll))
+  (benchmark-suite [coll (array 1 2 3)] (nth coll 2))
+  (benchmark-suite [coll "foobar"] (nth coll 2))
+
+  ;; cloning & specify
+  (benchmark-suite [coll [1 2 3]] (clone coll))
+  (benchmark-suite [coll [1 2 3]] (specify coll IFoo (foo [_] :bar)))
+  (benchmark-suite [coll (specify [1 2 3] IFoo (foo [_] :bar))] (foo coll))
+
+  ;; list ops
+  (benchmark-suite [coll (list 1 2 3)] (first coll))
+  (benchmark-suite [coll (list 1 2 3)] (-first coll))
+  (benchmark-suite [coll (list 1 2 3)] (rest coll))
+  (benchmark-suite [coll (list 1 2 3)] (-rest coll))
+  (benchmark-suite [] (list))
+  (benchmark-suite [] (list 1 2 3))
+
+  ;; vector ops
+  (benchmark-suite [] [])
+  (benchmark-suite [[a b c] (take 3 (repeatedly #(rand-int 10)))] (-count [a b c]))
+  (benchmark-suite [[a b c] (take 3 (repeatedly #(rand-int 10)))] (-count (vec #js [a b c])))
+  (benchmark-suite [[a b c] (take 3 (repeatedly #(rand-int 10)))] (-count (vector a b c)))
+  (benchmark-suite [coll [1 2 3]] (transient coll))
+  (benchmark-suite [coll [1 2 3]] (nth coll 0))
+  (benchmark-suite [coll [1 2 3]] (-nth coll 0))
+  (benchmark-suite [coll [1 2 3]] (-nth ^not-native coll 0))
+  (benchmark-suite [coll [1 2 3]] (coll 0))
+  (benchmark-suite [coll [1 2 3]] (conj coll 4))
+  (benchmark-suite [coll [1 2 3]] (-conj coll 4))
+  (benchmark-suite [coll []] (-conj ^not-native coll 1))
+  (benchmark-suite [coll [1]] (-conj ^not-native coll 2))
+  (benchmark-suite [coll [1 2]] (-conj ^not-native coll 3))
+  (benchmark-suite [coll [1 2 3]] (seq coll))
+  (benchmark-suite [coll [1 2 3]] (-seq coll))
+  (benchmark-suite [coll (seq [1 2 3])] (first coll))
+  (benchmark-suite [coll (seq [1 2 3])] (-first coll))
+  (benchmark-suite [coll (seq [1 2 3])] (rest coll))
+  (benchmark-suite [coll (seq [1 2 3])] (-rest coll))
+  (benchmark-suite [coll (seq [1 2 3])] (next coll))
 
   )
 
