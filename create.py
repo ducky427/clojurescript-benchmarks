f42ceb0f3b6f9a7478b96378225486d94cf7e10f
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-06-10 08:17:29 +0100

    Create the list of benchmarks from the csv file

--- create.py
+++ create.py
@@ -2,19 +2,22 @@
 import csv
 from jinja2 import Environment, FileSystemLoader
 
-items = ["[x 1] (identity x)",
-         "[] (symbol (quote foo))",
-         "[coll (seq arr)] (ci-reduce coll + 0)",
-         "[coll (seq arr)] (ci-reduce coll sum 0)",
-         "[coll arr] (array-reduce coll + 0)",
-         "[coll arr] (array-reduce coll sum 0)"]
-
-def main():
+def main(items):
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('index.tmpl')
 
     with open('index.html', 'wb') as f:
         f.write(template.render(items=items))
 
+def verify():
+    with open('data.csv') as f:
+        reader = csv.DictReader(f)
+        rows = list(reader)
+        assert (len(rows) % 3) == 0
+        first_version = rows[0]['Version']
+        first_engine = rows[0]['Engine']
+        return [x['Name'] for x in rows if (x['Version'] == first_version) and (x['Engine'] == first_engine)]
+
 if __name__ == '__main__':
-    main()
+    items = verify()
+    main(items)
