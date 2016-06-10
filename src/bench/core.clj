f399f9316d3abe5b2bf0ec380c4e756b0221bf9b
Rohit Aggarwal <rohit.aggarwal@gmail.com>
2016-06-10 09:12:10 +0100

    WIP

--- src/bench/core.clj
+++ src/bench/core.clj
@@ -19,9 +19,20 @@
   [bindings expr iterations]
   (let [title    (str (pr-str bindings) " " (pr-str expr))]
     `(let ~bindings
-       (let [start#   (cljs.core/system-time)]
-         (dotimes [_# ~iterations]
-           ~expr)
-         (~'println ~title " || "
-                    {"mean" (/ (- (cljs.core/system-time) start#) ~iterations)})
-         (~'println ~title ~iterations)))))
+       (if ^boolean bench.core/quick?
+         (let [start#   (.getTime (js/Date.))]
+           (dotimes [_# ~iterations]
+             ~expr)
+           (~'println ~title " || "
+            {"mean" (/ (- (.getTime (js/Date.)) start#) ~iterations)})
+           (~'println ~title ~iterations))
+         (let [suite# (bench.core/Suite.)]
+           (-> suite#
+               (.add ~title
+                     (fn [] ~expr))
+               (.on "cycle" (fn [event#]
+                              (let [b#    (-> event# .-target)
+                                    stat# (-> b# .-stats)]
+                                (~'println (.-name b#) (-> b# .-times .-elapsed) (.-count b#))
+                                (~'println (.-name b#) " || " (bench.core/get-metrics stat#)))))
+               (.run)))))))
